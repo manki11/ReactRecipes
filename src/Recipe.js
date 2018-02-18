9fdc1bded98fae4b9ebafc2fe66623be3477794c
Mankirat Singh <mankirat1998@gmail.com>
2018-02-19 00:28:39 +0530

    feat: initial structure of app

---
+++ src/Recipe.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+
+class Recipe extends Component{
+    render() {
+        return (
+            <div>Recipe</div>
+        );
+    }
+}
+
+export default Recipe;
---
+++ src/RecipeApp.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import Recipe from './Recipe'
+import './RecipeApp.css';
+
+class App extends Component {
+  render() {
+    return (
+      <Recipe/>
+    );
+  }
+}
+
+export default App;
